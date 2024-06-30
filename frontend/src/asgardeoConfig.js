794eb7aafc3e27dfabb99e09a8fcc1a94c4395b7
Romayle <creedtech77@gmail.com>
2024-06-30 23:32:55 +0530

    added asgardeo config json

--- frontend/src/asgardeoConfig.js
+++ frontend/src/asgardeoConfig.js
@@ -1,8 +1,10 @@
+require('dotenv').config() 
+
 const asgardeoConfig = {
     signInRedirectURL: "http://localhost:3000/",
     signOutRedirectURL: "http://localhost:3000/",
-    clientID: "01nazvj522A7Ibb5Rk2NOOeSZmYa",
-    baseUrl: "https://api.asgardeo.io/t/orgxg87f",
+    clientID: process.env.CLIENT_ID,
+    baseUrl: process.env.BASE_URL,
     scope: [ "openid","profile", "email" ],
     resourceServerURLs: ["https://bdc81b0c-bae6-43e8-b4aa-0702a82aee77-prod.e1-us-east-azure.choreoapis.dev/animetrix/movie-catalog-service/v1.0/categories", "https://bdc81b0c-bae6-43e8-b4aa-0702a82aee77-prod.e1-us-east-azure.choreoapis.dev/asgardeo-b2c-demo/accounts-management-api/endpoint-9090-803/v1.0/accounts"]
   };
