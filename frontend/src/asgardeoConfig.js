0399f9bc5144d15e055d6b9bcf69998945caf19e
Creed77Tech <72990039+Creed77Tech@users.noreply.github.com>
2024-07-01 13:26:50 +0530

    Update asgardeoConfig.js

--- frontend/src/asgardeoConfig.js
+++ frontend/src/asgardeoConfig.js
@@ -3,8 +3,8 @@
 const asgardeoConfig = {
     signInRedirectURL: "https://asgardeo-todo-gji2.vercel.app/",
     signOutRedirectURL: "https://asgardeo-todo-gji2.vercel.app/",
-    clientID: process.env.CLIENT_ID,
-    baseUrl: process.env.BASE_URL,
+    clientID: process.env.REACT_APP_CLIENT_ID,
+    baseUrl: process.env.REACT_APP_BASE_URL,
     scope: [ "openid","profile", "email" ],
     resourceServerURLs: ["https://bdc81b0c-bae6-43e8-b4aa-0702a82aee77-prod.e1-us-east-azure.choreoapis.dev/animetrix/movie-catalog-service/v1.0/categories", "https://bdc81b0c-bae6-43e8-b4aa-0702a82aee77-prod.e1-us-east-azure.choreoapis.dev/asgardeo-b2c-demo/accounts-management-api/endpoint-9090-803/v1.0/accounts"]
   };
